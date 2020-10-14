5bc769a8749df501bc1c2383db377402f8d86852
zastrixarundell <zasitrxarundell@gmail.com>
2020-10-14 12:54:40 +0200

    Added author model

---
+++ app/models/author.rb
@@ -0,0 +1,11 @@
+# == Schema Information
+#
+# Table name: authors
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Author < ApplicationRecord
+end
---
+++ test/models/author_test.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: authors
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require 'test_helper'
+
+class AuthorTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
