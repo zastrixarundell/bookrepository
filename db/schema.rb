71576dcaec2b1118287349d573e6215f62fce1b8
zastrixarundell <zasitrxarundell@gmail.com>
2020-10-14 12:56:54 +0200

    Created series model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_105317) do
+ActiveRecord::Schema.define(version: 2020_10_14_105616) do
 
   create_table "authors", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "series", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
---
+++ test/models/series_test.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: series
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require 'test_helper'
+
+class SeriesTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
