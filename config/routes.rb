3675b769bb2ca0b456496d679388881e020ed04b
zastrixarundell <zasitrxarundell@gmail.com>
2020-10-14 15:50:33 +0200

    Added Administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :books
+      resources :users
+      resources :series
+      resources :authors
+      resources :categories
+
+      root to: "books#index"
+    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
